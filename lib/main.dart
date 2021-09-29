5d2d797a13301bb1db75a5bc309a276a3a513d7b
FrazileDevelopers <frazile.com@gmail.com>
2021-09-29 12:11:57 +0530

    Bugs Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/cupertino.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'Colour Blindness/homepage1.dart';
 import 'visual acuity/instructions.dart';
-import 'visual acuity/option1.dart';
 import 'visual acuity/homepage.dart';
-import 'package:bottom_animation/bottom_animation.dart';
 
 void main() {
   runApp(HomePage());
@@ -28,35 +25,45 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            brightness: Brightness.light, primaryColor: Colors.blueGrey),
-        home: Scaffold(
-            body: IndexedStack(
-              index: currentIndex,
-              children: screens,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blueGrey,
+      ),
+      home: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: screens,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black54,
+          showUnselectedLabels: false,
+          selectedFontSize: 17,
+          unselectedFontSize: 12,
+          iconSize: 35,
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.assignment_outlined),
+              label: 'Visual Acuity',
             ),
-            bottomNavigationBar: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                backgroundColor: Colors.white,
-                selectedItemColor: Colors.black,
-                unselectedItemColor: Colors.black54,
-                showUnselectedLabels: false,
-                selectedFontSize: 17,
-                unselectedFontSize: 12,
-                iconSize: 35,
-                currentIndex: currentIndex,
-                onTap: (index) => setState(() => currentIndex = index),
-                items: [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.assignment_outlined),
-                      label: 'Visual Acuity'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.color_lens_outlined),
-                      label: 'Colour Blindness'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.medication_outlined),
-                      label: 'Eye Health'),
-                ])));
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.color_lens_outlined,
+              ),
+              label: 'Colour Blindness',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.medication_outlined),
+              label: 'Eye Health',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
